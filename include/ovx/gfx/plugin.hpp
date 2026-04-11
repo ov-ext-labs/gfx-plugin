375f18c56b0b57832fec8244c201c517bc92a13b
Alexander Nesterov <nesterov.alexander@outlook.com>
2026-04-11 13:08:46 +0200

    Import gfx_plugin from openvino_contrib PR 1050

--- include/ovx/gfx/plugin.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace ovx::gfx {
-
-std::string plugin_name();
-
-}  // namespace ovx::gfx
--- src/plugin.cpp
+++
@@ -1,9 +0,0 @@
-#include "ovx/gfx/plugin.hpp"
-
-namespace ovx::gfx {
-
-std::string plugin_name() {
-    return "ovx_gfx_plugin";
-}
-
-}  // namespace ovx::gfx
